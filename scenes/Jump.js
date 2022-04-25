89bb6b2c11831c0140c2204437ac81aebd7f8c41
JimWhiteheadUCSC <ejw@soe.ucsc.edu>
2022-04-24 22:30:29 -0700

    Creating initial setup

--- scenes/Jump.js
+++ scenes/Jump.js
@@ -9,7 +9,7 @@ class Jump extends Phaser.Scene {
         this.MAX_X_VEL = 500;   // pixels/second
         this.MAX_Y_VEL = 5000;
         this.DRAG = 600;    // DRAG < ACCELERATION = icy slide
-        this.physics.world.gravity.y = 10000;
+        this.physics.world.gravity.y = 3000;
 
         // set bg color
         this.cameras.main.setBackgroundColor('#227B96');
@@ -108,7 +108,7 @@ class Jump extends Phaser.Scene {
         if(this.alien.body.touching.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
             // set jump velocity here
 
-            
+
             this.upKey.tint = 0xFACADE;
         } else {
             this.upKey.tint = 0xFFFFFF;
